b4dda60b9eab409d0085e25630271fd14681d952
uberto.barbini <uberto.barbini@adfonic.com>
2012-12-13 14:02:25 +0000

    test four passed

--- src/test/java/com/gamasoft/kata/RomansTest.java
+++ src/test/java/com/gamasoft/kata/RomansTest.java
@@ -8,29 +8,37 @@ import static org.junit.Assert.assertThat;
 public class RomansTest {
 
     @Test
-    public void oneIsI(){
+    public void oneIsI() {
         assertThat(romanOne(), is("I"));
     }
 
     @Test
-    public void twoIsII(){
+    public void twoIsII() {
         assertThat(romanOnes(2), is("II"));
     }
 
     @Test
-    public void threeIsIII(){
+    public void threeIsIII() {
         assertThat(romanOnes(3), is("III"));
     }
 
     @Test
-    public void fourIsIV(){
-        assertThat(romanOnes(4), is("III"));
+    public void fourIsIV() {
+
+        int number = 4;
+        String roman = "";
+        if (number == 4) {
+            roman = romanOnes(1) + "V";
+        } else {
+            roman = romanOnes(number);
+        }
+        assertThat(roman, is("IV"));
     }
 
     private String romanOnes(int number) {
         String two = "";
         for (int i = 0; i < number; i++) {
-           two += romanOne();
+            two += romanOne();
         }
         return two;
     }
