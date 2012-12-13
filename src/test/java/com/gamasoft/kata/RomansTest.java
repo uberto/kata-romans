a9468aef63a31371ff6eeaec563924c32de7dc96
uberto.barbini <uberto.barbini@adfonic.com>
2012-12-13 12:49:33 +0000

    first failing test

---
+++ src/test/java/com/gamasoft/kata/RomansTest.java
@@ -0,0 +1,15 @@
+package com.gamasoft.kata;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class RomansTest {
+
+    @Test
+    public void oneIsI(){
+        String roman = "";
+        assertThat(roman, is("I"));
+    }
+}
